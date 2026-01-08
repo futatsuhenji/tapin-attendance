2e47292ac8d6d682d4a639f2f4985c37c551f4df
KATO Hayate <dev@hayatek.jp>
2026-01-08 11:37:56 +0900

    prismaラッパーを作成

---
+++ src/lib/prisma.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@/generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+
+
+const globalForPrisma = globalThis as unknown as {
+    prisma?: PrismaClient;
+};
+
+
+export const prisma =
+    globalForPrisma.prisma ??
+    (() => {
+        const pool = new Pool({
+            connectionString: process.env.DATABASE_URL,
+        });
+
+        const adapter = new PrismaPg(pool);
+
+        return new PrismaClient({
+            adapter,
+            log: process.env.NODE_ENV !== 'production' ? ['info', 'warn', 'error'] : ['query', 'info', 'warn', 'error'],
+        });
+    })();
+
+
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma;
+}
