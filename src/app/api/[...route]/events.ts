7e5b898ece134607b1c7cf6f0bec5d94ea375d22
KATO Hayate <dev@hayatek.jp>
2026-01-09 17:30:33 +0900

    不要なAPIを削除

--- src/app/api/[...route]/events.ts
+++ src/app/api/[...route]/events.ts
@@ -1,10 +1,8 @@
 import { Hono } from 'hono';
 
 import manage from './events/manage';
-import ping from './ping';
 
 const app = new Hono()
-    .route('/:groupId/:eventId/manage', manage)
-    .route('/ping', ping);
+    .route('/:groupId/:eventId/manage', manage);
 
 export default app;
