eabccd8b6624f6a2cb9d7d704661db987e12044c
Yu Yokoyama <25w6105e@shinshu-u.ac.jp>
2026-01-11 00:40:10 +0900

    rename: create.ts を management.ts にリネーム

--- src/app/api/[...route]/events.ts
+++ src/app/api/[...route]/events.ts
@@ -2,13 +2,13 @@ import { Hono } from 'hono';
 
 import manage from './events/manage';
 import respond from './events/respond';
-import create from './events/create';
+import management from './events/management';
 import detail from './events/detail';
 
 const app = new Hono()
     .route('/:groupId/:eventId/manage', manage)
     .route('/:groupId/:eventId/respond', respond)
-    .route('/:groupId', create)
+    .route('/:groupId', management)
     .route('/:groupId/:eventId', detail);
 
 export default app;
